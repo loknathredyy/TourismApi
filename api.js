0f18d6666ce47eb18fe6457378b06d1eb22e9c85
K Loknath <88827089+loknathredyy@users.noreply.github.com>
2022-04-13 14:54:41 +0530

    Update api.js

--- api.js
+++ api.js
@@ -9,7 +9,8 @@ app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
 //   res.setHeader('Access-Control-Allow-Origin', 'https://loknath-tourism-app.vercel.app', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Origin', 'https://loknath-tourism-app.vercel.app', 'http://localhost:3000');
+//   res.setHeader('Access-Control-Allow-Origin', 'https://loknath-tourism-app.vercel.app', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://loknath-tourism-app.vercel.app', 'http://localhost:3000', 'https://indian-tourism-app.herokuapp.com');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
